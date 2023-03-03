bcbd826ddd90e1e5eaa01f9848d3aaa51599febb
clxxiii <clxxiii@users.noreply.github.com>
2023-03-03 11:40:12 -0500

    fix pager

--- src/routes/videos/+page.server.ts
+++ src/routes/videos/+page.server.ts
@@ -41,7 +41,21 @@ export const load = (async ({ cookies, url }) => {
 			take: 30,
 			skip: 30 * (page - 1)
 		});
-		return { videos, user: 'admin', page };
+
+		const allvideos = (
+			await prisma.video.findMany({
+				where: {
+					NOT: {
+						id: 'this user has watched all videos'
+					}
+				},
+				select: {
+					id: true
+				}
+			})
+		).length;
+		const max = Math.ceil(allvideos / 30);
+		return { videos, user: 'admin', page, max };
 	}
 
 	const videos = await prisma.video.findMany({
@@ -65,5 +79,18 @@ export const load = (async ({ cookies, url }) => {
 		skip: 30 & (page - 1)
 	});
 
-	return { videos, user: 'default', page };
+	const max = (
+		await prisma.video.findMany({
+			where: {
+				user: {
+					id: session.stats.user.id
+				}
+			},
+			select: {
+				id: true
+			}
+		})
+	).length;
+
+	return { videos, user: 'default', page, max };
 }) satisfies ServerLoad;
