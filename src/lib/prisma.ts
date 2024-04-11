e76d7d540f503406e4af62f9169926054b3f5dfc
clxxiii <clxxiii@users.noreply.github.com>
2024-04-11 15:14:38 -0400

    use turso for database

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
+
+const libsql = createClient({
+  url: `${process.env.DATABASE_URL}`,
+  authToken: `${process.env.TURSO_TOKEN}`,
+})
+
+const adapter = new PrismaLibSQL(libsql)
+const prisma = new PrismaClient({ adapter });
 export { prisma };
